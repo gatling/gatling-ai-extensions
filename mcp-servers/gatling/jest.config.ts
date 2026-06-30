28b7ca368ab1402d8d31ab8ad9ecc1d8d7e47a3e
Guillaume Corré <gui.corre@gmail.com>
2026-06-30 10:39:49 +0200

    chore: refactor tests

--- mcp-servers/gatling/jest.config.ts
+++ mcp-servers/gatling/jest.config.ts
@@ -1,10 +1,20 @@
 import type { Config } from "jest";
 import { createJsWithTsPreset } from "ts-jest";
 
+const tsJestTransformCfg = createJsWithTsPreset({
+  diagnostics: {
+    ignoreCodes: [151002]
+  }
+}).transform;
+
 export default {
-  ...createJsWithTsPreset({
-    diagnostics: {
-      ignoreCodes: [151002]
-    }
-  })
+  moduleNameMapper: {
+    "@src/(.*)\\.js$": "<rootDir>/$1"
+  },
+  modulePathIgnorePatterns: ["src/index.test.ts"],
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  rootDir: "src"
 } satisfies Config;
